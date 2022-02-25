98c66a4d029ef96d14018ede6d760abb277fbd19
MarwaneAlHaddaoui <100091401+MarwaneAlh@users.noreply.github.com>
2022-02-25 14:21:35 +0100

    Gestion des mouvements
    Snake capable de bouger à l'aide des touches directionnels

--- Snake/Assets/SnakeGameScript.cs
+++ Snake/Assets/SnakeGameScript.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SnakeGameScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   /*Utilisation d'attribut de type Vector , type qui contient deux valeurs comme une paire
+   /*Cette value va servire a stocker l'axe X ainsi que l'xe Y de la direction 
+   /*Initialisation a la direction right
+   */
+   private Vector2 _direction = Vector2.right;
+
+   /*Methode update qui va etre appellé en permance afin de gerer le jeux
+   /*Check quelle touche du clavier entré , ici l'ont joue avec les fleches pour en changer la direction
+   /*Possibilité de le changer en switch case(a ameliorer)
+   */
+   private void Update()
+   {
+      if(Input.GetKeyDown(KeyCode.UpArrow)){
+         _direction=Vector2.up;
+      }else if(Input.GetKeyDown(KeyCode.DownArrow)){
+         _direction=Vector2.down;
+      }else if(Input.GetKeyDown(KeyCode.LeftArrow)){
+         _direction=Vector2.left;
+      }else if(Input.GetKeyDown(KeyCode.RightArrow)){
+         _direction=Vector2.right;
+
+   }
+   }
+
+   /*Creattion de cette methode afin d'etre appelé dans un laps de temps
+   /*Acces a la propriété transform de l'objet afin de lajouter a la value direction
+   /*La methode Mathf.Round permet d'arrondir la position 
+   */
+   private void FixedUpdate(){
+      this.transform.position = new Vector3(
+         Mathf.Round(this.transform.position.x)+_direction.x,
+         Mathf.Round(this.transform.position.y)+_direction.y,
+         0.0f
+         );
+   }
 }
