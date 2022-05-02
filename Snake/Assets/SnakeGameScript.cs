e085e1bcaf9f976042b72f2fe7142b16a18d8e34
Al haddaoui Marwane <100091401+MarwaneAlh@users.noreply.github.com>
2022-05-02 18:32:00 +0200

    Ajout du boutton pause et resumé

--- Snake/Assets/SnakeGameScript.cs
+++ Snake/Assets/SnakeGameScript.cs
@@ -6,84 +6,112 @@ using UnityEngine.Audio;
 
 public class SnakeGameScript : MonoBehaviour
 {
-   
-    
 
 
-   /*Utilisation d'attribut de type Vector , type qui contient deux valeurs comme une paire
-   /*Cette value va servire a stocker l'axe X ainsi que l'xe Y de la direction 
-   /*Initialisation a la direction right
-   *Latribut body ici represente son corps
-   *Attribut Score ici representant le score
-   *L'attribut AudioSource ici represente le fichier audio lorsqu'un joueur mange le fruit
-   *L'attribut Text sert à fair une liaison avec le text UI 
-   */
-   private Vector2 _direction = Vector2.right;
-   private List<Transform> _body;
+
+
+    /*Utilisation d'attribut de type Vector , type qui contient deux valeurs comme une paire
+    /*Cette value va servire a stocker l'axe X ainsi que l'xe Y de la direction 
+    /*Initialisation a la direction right
+    *Latribut body ici represente son corps
+    *Attribut Score ici representant le score
+    *L'attribut AudioSource ici represente le fichier audio lorsqu'un joueur mange le fruit
+    *L'attribut Text sert à fair une liaison avec le text UI 
+    *L'attribut isPaused sert a gerer l'action pause et start du jeux
+    */
+    private Vector2 _direction = Vector2.right;
+    private List<Transform> _body;
     public Transform bodyPrefab;
     public static int score = 0;
     public AudioSource eatsounds;
     public Text textscore;
+    bool isPaused = false;
+
+
+
+
+    public void pauseGame()
+    {
+
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+    }
 
     /*Methode Game Over servant a generer l'ecran en cas de perte de jeux
      *
      */
 
-  
+
     public void GameOver()
     {
-       
+
         SceneManager.LoadScene("GameOverScene");
     }
-    private void Start() 
+    private void Start()
     {
-        textscore=GameObject.Find("Canvas/Text").GetComponent<Text>();
+        textscore = GameObject.Find("Canvas/Text").GetComponent<Text>();
         _body = new List<Transform>();
-        _body.Add(this.transform);  
+        _body.Add(this.transform);
     }
 
-   /*Methode update qui va etre appellé en permance afin de gerer le jeux
-   /*Check quelle touche du clavier entré , ici l'ont joue avec les fleches pour en changer la direction
-   /*Possibilité de le changer en switch case(a ameliorer)
-   */
-   private void Update()
-   {
-
-      if(Input.GetKeyDown(KeyCode.UpArrow)){
-         _direction=Vector2.up;
-      }else if(Input.GetKeyDown(KeyCode.DownArrow)){
-         _direction=Vector2.down;
-      }else if(Input.GetKeyDown(KeyCode.LeftArrow)){
-         _direction=Vector2.left;
-      }else if(Input.GetKeyDown(KeyCode.RightArrow)){
-         _direction=Vector2.right;
-
-   }
-   }
-
-   /*Creattion de cette methode afin d'etre appelé dans un laps de temps
-   /*Acces a la propriété transform de l'objet afin de lajouter a la value direction
-   /*La methode Mathf.Round permet d'arrondir la position 
-   *Ajout de la partie qui va coller le corps du serpent a sa tete dans la boucle for
+    /*Methode update qui va etre appellé en permance afin de gerer le jeux
+    /*Check quelle touche du clavier entré , ici l'ont joue avec les fleches pour en changer la direction
+    /*Possibilité de le changer en switch case(a ameliorer)
     */
-   private void FixedUpdate(){
+    private void Update()
+    {
 
-        for(int i = _body.Count - 1; i > 0; i--)
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _direction = Vector2.right;
+
+        }
+    }
+
+    /*Creattion de cette methode afin d'etre appelé dans un laps de temps
+    /*Acces a la propriété transform de l'objet afin de lajouter a la value direction
+    /*La methode Mathf.Round permet d'arrondir la position 
+    *Ajout de la partie qui va coller le corps du serpent a sa tete dans la boucle for
+     */
+    private void FixedUpdate()
+    {
+
+        for (int i = _body.Count - 1; i > 0; i--)
         {
             _body[i].position = _body[i - 1].position;
         }
-      this.transform.position = new Vector3(
-         Mathf.Round(this.transform.position.x)+_direction.x,
-         Mathf.Round(this.transform.position.y)+_direction.y,
-         0.0f
-         );
-   }
+        this.transform.position = new Vector3(
+           Mathf.Round(this.transform.position.x) + _direction.x,
+           Mathf.Round(this.transform.position.y) + _direction.y,
+           0.0f
+           );
+    }
     /*Methode creer pour gerer lagrandissement du serpent
      * Ici un objet transform créer sur unity est instancer 
      *La position est donnée grace au derniere element de la liste du body 
      */
-    private void Grow() 
-        
+    private void Grow()
+
     {
         score += 10;
         Transform body = Instantiate(this.bodyPrefab);
@@ -99,18 +127,18 @@ public class SnakeGameScript : MonoBehaviour
      */
 
     private void ResetState()
-        
+
     {
-        
-        
-       GameOver();
-        for (int i = 1; i < _body.Count;i++)
+
+
+        GameOver();
+        for (int i = 1; i < _body.Count; i++)
         {
             Destroy(_body[i].gameObject);
         }
         _body.Clear();
         _body.Add(this.transform);
-        this.transform.position = Vector3.zero; 
+        this.transform.position = Vector3.zero;
     }
 
     /*Methode creer pour automatiquement ajouter de la longueru
@@ -118,24 +146,25 @@ public class SnakeGameScript : MonoBehaviour
      */
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag=="Fruit")
+        if (other.tag == "Fruit")
         {
             Grow();
-            Debug.Log("Score :  " +score);
+            Debug.Log("Score :  " + score);
             textscore.text = "Score : " + score;
             eatsounds.Play();
         }
-        if (other.tag=="Bonus")
+        if (other.tag == "Bonus")
         {
             score += 50;
             textscore.text = "Score : " + score;
         }
-        else if (other.tag == "Wall"||other.tag=="poison")
+        else if (other.tag == "Wall" || other.tag == "poison")
         {
-          
-            ResetState();   
-        }
-    }   
 
-   
+            ResetState();
+        }
+    }
 }
+
+
+    
